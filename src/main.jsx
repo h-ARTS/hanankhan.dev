58074151da889b50707f53808860dd418ac7b9a9
Hanan Mufti <hi5hanan@yahoo.com>
2023-12-29 19:33:33 +0100

    add lenis config

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,17 @@ import Lenis from "@studio-freight/lenis"
 import "bootstrap/dist/css/bootstrap-grid.min.css"
 import "./index.css"
 
-const lenis = new Lenis()
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+    direction: "vertical",
+    gestureDirection: "vertical",
+    smooth: true,
+    mouseMultiplier: 1,
+    smoothTouch: false,
+    touchMultiplier: 2,
+    infinite: false,
+})
 
 function raf(time) {
     lenis.raf(time)
