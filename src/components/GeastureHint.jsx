04178a6bce2de13c9d4758eebc7de8d47fd8369e
Hanan Mufti <hi5hanan@yahoo.com>
2023-12-29 19:33:40 +0100

    add pulse effect

--- src/components/GeastureHint.jsx
+++ src/components/GeastureHint.jsx
@@ -4,6 +4,7 @@ import swipeUpSvg from "../assets/swipe-up.svg"
 
 const GestureHint = () => (
     <button className="gesture-hint">
+        <div className="dot"></div>
         <img src={swipeUpSvg} alt="swipe up" />
         <h5>SWIPE UP</h5>
     </button>
